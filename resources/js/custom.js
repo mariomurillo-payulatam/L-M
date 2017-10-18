4d699dd32f1b40cb3050d5d9f1dfa0ef7f365feb
mariomurillo-payulatam <mario.murillo@payulatam.com>
2017-10-17 22:48:31 -0500

    Se agrega slideshow

--- resources/js/custom.js
+++ resources/js/custom.js
@@ -1,14 +1,14 @@
-var slideIndex = 0;
-showSlides();
+var myIndex = 0;
+carousel();
 
-function showSlides() {
+function carousel(elementName) {
     var i;
-    var slides = document.getElementsByClassName("mySlides");
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none"; 
+    var x = document.getElementsByClassName("mySlides");
+    for (i = 0; i < x.length; i++) {
+       x[i].style.display = "none";  
     }
-    slideIndex++;
-    if (slideIndex > slides.length) {slideIndex = 1} 
-    slides[slideIndex-1].style.display = "block"; 
-    setTimeout(showSlides, 1000); // Change image every 2 seconds
+    myIndex++;
+    if (myIndex > x.length) {myIndex = 1}    
+    x[myIndex-1].style.display = "block";  
+    setTimeout(carousel, 2000); // Change image every 2 seconds
 }
